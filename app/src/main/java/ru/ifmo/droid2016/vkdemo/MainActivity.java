34c9b248516d92a7d33c009c4abfd48a32b64899
Trunin Dmitry <dmitry.trunin@corp.mail.ru>
2016-10-19 10:23:00 +0300

    Print fingerprint to log.

--- app/src/main/java/ru/ifmo/droid2016/vkdemo/MainActivity.java
+++ app/src/main/java/ru/ifmo/droid2016/vkdemo/MainActivity.java
@@ -3,15 +3,22 @@ package ru.ifmo.droid2016.vkdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.util.VKUtil;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String[] fingerprints = VKUtil.getCertificateFingerprint(this, this.getPackageName());
+        Log.d("VkDemo", "Fingerprints: " + Arrays.toString(fingerprints));
 
         if (VKAccessToken.tokenFromSharedPreferences(this, Constants.KEY_TOKEN) != null) {
             startVkDemo();
